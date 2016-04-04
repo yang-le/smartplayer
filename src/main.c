4e2dcdf31382e2316989cfa6588fb39ece67c4df
Yang Le <yangle0125@qq.com>
2016-04-04 14:14:56 +0800

    sound ok

--- src/main.c
+++ src/main.c
@@ -25,8 +25,7 @@ static struct SwsContext *img_convert_ctx = NULL;
 
 static int width = 0, height = 0;
 static enum AVPixelFormat pix_fmt = AV_PIX_FMT_NONE;
-static AVFrame *frame = NULL, *frame_YUV = NULL;
-static AVPacket *pkt = NULL;
+static AVFrame *frame_audio = NULL, *frame_video = NULL, *frame_YUV = NULL;
 static int video_frame_count = 0;
 static int audio_frame_count = 0;
 
@@ -146,15 +145,15 @@ static int decode_packet(AVPacket *pkt/*, int *got_frame*/)
 
     if (pkt->stream_index == video_stream_idx) {
         /* decode video frame */
-        ret = avcodec_decode_video2(video_dec_ctx, frame, got_frame, pkt);
+        ret = avcodec_decode_video2(video_dec_ctx, frame_video, got_frame, pkt);
         if (ret < 0) {
             fprintf(stderr, "Error decoding video frame (%s)\n", av_err2str(ret));
             return ret;
         }
 
         if (*got_frame) {
-            if (frame->width != width || frame->height != height ||
-                frame->format != pix_fmt) {
+            if (frame_video->width != width || frame_video->height != height ||
+                frame_video->format != pix_fmt) {
                 /* To handle this change, one could call av_image_alloc again and
                  * decode the following frames into another rawvideo file. */
                 fprintf(stderr, "Error: Width, height and pixel format have to be "
@@ -163,16 +162,16 @@ static int decode_packet(AVPacket *pkt/*, int *got_frame*/)
                         "old: width = %d, height = %d, format = %s\n"
                         "new: width = %d, height = %d, format = %s\n",
                         width, height, av_get_pix_fmt_name(pix_fmt),
-                        frame->width, frame->height,
-                        av_get_pix_fmt_name(frame->format));
+                        frame_video->width, frame_video->height,
+                        av_get_pix_fmt_name(frame_video->format));
                 return -1;
             }
 
             debug_info("video_frame n:%d coded_n:%d pts:%s\n",
-                   video_frame_count++, frame->coded_picture_number,
-                   av_ts2timestr(frame->pts, &video_dec_ctx->time_base));
+                   video_frame_count++, frame_video->coded_picture_number,
+                   av_ts2timestr(frame_video->pts, &video_dec_ctx->time_base));
 
-		sws_scale(img_convert_ctx, (const uint8_t * const*)frame->data, frame->linesize, 0, height,	
+		sws_scale(img_convert_ctx, (const uint8_t * const*)frame_video->data, frame_video->linesize, 0, height,	
 			frame_YUV->data, frame_YUV->linesize);
 
 		SDL_UpdateYUVTexture(sdlTexture, &sdlRect,	
@@ -188,7 +187,7 @@ static int decode_packet(AVPacket *pkt/*, int *got_frame*/)
 
     if (pkt->stream_index == audio_stream_idx) {
         /* decode audio frame */
-        ret = avcodec_decode_audio4(audio_dec_ctx, frame, got_frame, pkt);
+        ret = avcodec_decode_audio4(audio_dec_ctx, frame_audio, got_frame, pkt);
         if (ret < 0) {
             fprintf(stderr, "Error decoding audio frame (%s)\n", av_err2str(ret));
             return ret;
@@ -200,10 +199,11 @@ static int decode_packet(AVPacket *pkt/*, int *got_frame*/)
         decoded = FFMIN(ret, pkt->size);
 
         if (*got_frame) {
-            size_t unpadded_linesize = frame->nb_samples * av_get_bytes_per_sample(frame->format);
-            debug_info("audio_frame n:%d nb_samples:%d pts:%s\n",
-                   audio_frame_count++, frame->nb_samples,
-                   av_ts2timestr(frame->pts, &audio_dec_ctx->time_base));
+            size_t unpadded_linesize = frame_audio->nb_samples * av_get_bytes_per_sample(frame_audio->format);
+		frame_audio->linesize[0] = unpadded_linesize;	// for test
+		debug_info("audio_frame n:%d nb_samples:%d pts:%s\n",
+                   audio_frame_count++, frame_audio->nb_samples,
+                   av_ts2timestr(frame_audio->pts, &audio_dec_ctx->time_base));
 
             /* Write the raw audio data samples of the first plane. This works
              * fine for packed formats (e.g. AV_SAMPLE_FMT_S16). However,
@@ -213,12 +213,7 @@ static int decode_packet(AVPacket *pkt/*, int *got_frame*/)
              * in these cases.
              * You should use libswresample or libavfilter to convert the frame
              * to packed data. */
-            //fwrite(frame->extended_data[0], 1, unpadded_linesize, audio_dst_file);
-            ret = SDL_QueueAudio(1, frame->extended_data[0], unpadded_linesize);
-		if (ret < 0) {
-			fprintf(stderr, "SDL_QueueAudio: %s\n", SDL_GetError());
-			return ret;
-		}
+            //fwrite(frame_audio->extended_data[0], 1, unpadded_linesize, audio_file);
         }
     }
 
@@ -227,6 +222,13 @@ static int decode_packet(AVPacket *pkt/*, int *got_frame*/)
 
 int decode_thread(void *opaque)\
 {
+	/* initialize packet, let the demuxer fill it */
+	AVPacket *pkt = av_packet_alloc();
+	if (!pkt) {
+		fprintf(stderr, "Could not allocate packet\n");
+		return AVERROR(ENOMEM);
+	}
+
 	/* read frames from the file */
 	while (av_read_frame(fmt_ctx, pkt) >= 0) {
 		if(pkt->stream_index == video_stream_idx) {
@@ -238,32 +240,20 @@ int decode_thread(void *opaque)\
 		}
 	}
 
+	av_packet_free(&pkt);
 	return 0;
 }
 
-//Refresh Event  
-#define SFM_REFRESH_EVENT  (SDL_USEREVENT + 1) 
-#define SFM_AUDIO_EVENT  (SDL_USEREVENT + 2)  
-#define SFM_BREAK_EVENT  (SDL_USEREVENT + 3)  
-
-static Uint32 fire_audio_event(Uint32 interval, void *opaque)
-{  
-    SDL_Event event;  
-    event.type = SFM_AUDIO_EVENT;  
-    SDL_PushEvent(&event);  
-   
-    return interval;  
-} 
-
+// Event  
+#define USR_VIDEO_EVENT  (SDL_USEREVENT + 1) 
 static Uint32 fire_video_event(Uint32 interval, void *opaque)
 {  
     SDL_Event event;  
-    event.type = SFM_REFRESH_EVENT;  
+    event.type = USR_VIDEO_EVENT;  
     SDL_PushEvent(&event);  
    
     return interval;  
 } 
-
 
 static void sdl_event_loop()
 {
@@ -273,18 +263,12 @@ static void sdl_event_loop()
 		SDL_Event event;
 		SDL_WaitEvent(&event);
 		
-		if(event.type==SFM_REFRESH_EVENT){
+		if (event.type == USR_VIDEO_EVENT) {
 			if (!thread_pause) {
 				// read from video_queue and paint
-				packet_queue_get(&video_queue, pkt);
-				decode_packet(pkt);
-				av_packet_unref(pkt);
-			}
-		} else if(event.type==SFM_AUDIO_EVENT) {
-			if (!thread_pause) {
-				packet_queue_get(&audio_queue, pkt);
-				decode_packet(pkt);
-				av_packet_unref(pkt);
+				AVPacket video_pkt;
+				packet_queue_get(&video_queue, &video_pkt);
+				decode_packet(&video_pkt);
 			}
 		} else if(event.type==SDL_KEYDOWN) {	
 			//Pause  
@@ -294,10 +278,86 @@ static void sdl_event_loop()
 			}
 		} else if(event.type==SDL_QUIT) {  
 			break;	
-		} else if(event.type==SFM_BREAK_EVENT) {	
-			break;	
-		} 
+		}
 	}
+}
+
+static double get_stream_fps(const AVStream *s)
+{
+	AVRational a_fps = s->avg_frame_rate;
+	if (a_fps.den && a_fps.num) {
+		return av_q2d(a_fps);
+	}
+
+	AVRational r_fps = av_stream_get_r_frame_rate(s);
+	if (r_fps.den && r_fps.num) {
+		return av_q2d(r_fps);
+	}
+
+	return 25.0f;
+}
+
+static void feed_audio(void *userdata, Uint8 *stream, int len)
+{
+	PacketQueue *queue = (PacketQueue *)userdata;
+
+	unsigned int *size = &frame_audio->linesize[0];
+	unsigned int *pos = &frame_audio->linesize[1];	// we use this to store pos info
+
+	SDL_memset(stream, 0, len);
+
+	while (len > 0){
+		if (*pos >= *size) { //already send all our data, get more
+			AVPacket audio_pkt;
+			packet_queue_get(queue, &audio_pkt);
+			decode_packet(&audio_pkt);
+			*pos = 0;
+		}
+
+		int my_len = (*size > 0 ? *size : 0) - *pos;
+		if (my_len > len) {
+			my_len = len;
+		}
+
+		Uint8 *buf = frame_audio->extended_data[0];		
+		SDL_MixAudioFormat(stream, &buf[*pos], AUDIO_S16SYS, len, SDL_MIX_MAXVOLUME);
+
+		len -= my_len;
+		stream += my_len;
+		*pos += my_len;
+	}
+}
+
+static SDL_AudioFormat get_format(enum AVSampleFormat sample_fmt)
+{
+    int i = 0;
+    SDL_AudioFormat fmt = 0;
+    struct sample_fmt_entry {
+        enum AVSampleFormat sample_fmt; SDL_AudioFormat fmt_be, fmt_le;
+    } sample_fmt_entries[] = {
+        { AV_SAMPLE_FMT_U8,  AUDIO_U8, AUDIO_U8 },
+        { AV_SAMPLE_FMT_S16, AUDIO_S16MSB, AUDIO_S16LSB },
+        { AV_SAMPLE_FMT_S32, AUDIO_S32MSB, AUDIO_S32LSB },
+        { AV_SAMPLE_FMT_FLT, AUDIO_F32MSB, AUDIO_F32LSB },
+        
+        { AV_SAMPLE_FMT_U8P,  AUDIO_U8, AUDIO_U8 },
+        { AV_SAMPLE_FMT_S16P, AUDIO_S16MSB, AUDIO_S16LSB },
+        { AV_SAMPLE_FMT_S32P, AUDIO_S32MSB, AUDIO_S32LSB },
+        { AV_SAMPLE_FMT_FLTP, AUDIO_F32MSB, AUDIO_F32LSB }
+    };
+
+    for (i = 0; i < FF_ARRAY_ELEMS(sample_fmt_entries); i++) {
+        struct sample_fmt_entry *entry = &sample_fmt_entries[i];
+        if (sample_fmt == entry->sample_fmt) {
+            fmt = AV_NE(entry->fmt_be, entry->fmt_le);
+            return fmt;
+        }
+    }
+
+    fprintf(stderr,
+            "sample format %s is not supported as output format\n",
+            av_get_sample_fmt_name(sample_fmt));
+    return fmt;
 }
 
 int main(int argc, char *argv[])
@@ -333,6 +393,15 @@ int main(int argc, char *argv[])
 		goto end;
 	}
 
+	frame_audio = av_frame_alloc();
+	frame_video = av_frame_alloc();
+	frame_YUV = av_frame_alloc();
+	if (!frame_audio || !frame_video || !frame_YUV) {
+		fprintf(stderr, "Could not allocate frame\n");
+		ret = AVERROR(ENOMEM);
+		goto end;
+	}
+
 	if (open_codec_context(&video_stream_idx, fmt_ctx, AVMEDIA_TYPE_VIDEO) >= 0) {
 		video_stream = fmt_ctx->streams[video_stream_idx];
 		video_dec_ctx = video_stream->codec;
@@ -361,9 +430,11 @@ int main(int argc, char *argv[])
 
 		// Set audio settings from codec info
 		wanted_spec.freq = audio_dec_ctx->sample_rate;
-		wanted_spec.format = AUDIO_S16SYS;
+		wanted_spec.format = get_format(audio_dec_ctx->sample_fmt);
 		wanted_spec.channels = audio_dec_ctx->channels;
-		wanted_spec.samples = 1024;
+		wanted_spec.samples = audio_dec_ctx->frame_size;
+		wanted_spec.callback = feed_audio;
+		wanted_spec.userdata = &audio_queue;
 
 		if (SDL_OpenAudio(&wanted_spec, &spec) < 0) {
 			fprintf(stderr, "SDL_OpenAudio: %s\n", SDL_GetError());
@@ -384,14 +455,6 @@ int main(int argc, char *argv[])
 		goto end;
 	}
 
-	frame = av_frame_alloc();
-	frame_YUV = av_frame_alloc();
-	if (!frame || !frame_YUV) {
-		fprintf(stderr, "Could not allocate frame\n");
-		ret = AVERROR(ENOMEM);
-		goto end;
-	}
-
 	unsigned char *out_buffer=(unsigned char *)av_malloc(av_image_get_buffer_size(AV_PIX_FMT_YUV420P, width, height, 1));  
 	if (!out_buffer) {
 		fprintf(stderr, "Could not allocate out_buffer\n");
@@ -401,14 +464,6 @@ int main(int argc, char *argv[])
 
 	av_image_fill_arrays(frame_YUV->data, frame_YUV->linesize,out_buffer, AV_PIX_FMT_YUV420P,width, height,1);
 
-	/* initialize packet, let the demuxer fill it */
-	pkt = av_packet_alloc();
-	if (!pkt) {
-		fprintf(stderr, "Could not allocate packet\n");
-		ret = AVERROR(ENOMEM);
-		goto end;
-	}
-
 	packet_queue_init(&video_queue);
 	packet_queue_init(&audio_queue);
 
@@ -417,8 +472,7 @@ int main(int argc, char *argv[])
 	if (audio_stream)
 		debug_info("Demuxing audio from file '%s'\n", infile);
 
-	SDL_AddTimer(40, fire_video_event, NULL);	// interval to be calc from fps
-	SDL_AddTimer(50, fire_audio_event, NULL);	// interval to be calc from audio info
+	SDL_AddTimer(1000 / get_stream_fps(video_stream), fire_video_event, NULL);
 	SDL_CreateThread(decode_thread,NULL,NULL);
 
 	sdl_event_loop();
@@ -428,8 +482,8 @@ int main(int argc, char *argv[])
 end:
 	SDL_Quit();
 
-	av_packet_free(&pkt);
-	av_frame_free(&frame);
+	av_frame_free(&frame_audio);
+	av_frame_free(&frame_video);
 	av_frame_free(&frame_YUV);
 
 	avcodec_close(audio_dec_ctx);
